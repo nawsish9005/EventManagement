7dd8d8f336e3b63b38dda700dc120aeb19ffab91
nawsish <nawsish2018@gmail.com>
2025-06-15 10:51:07 +0600

    upd

--- EventBookingSystem/EventBookingSystem/obj/Debug/net8.0/EventBookingSystem.AssemblyInfo.cs
+++ EventBookingSystem/EventBookingSystem/obj/Debug/net8.0/EventBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EventBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38dc721e6c37a9f4ab10be2590b2a8981fa0e0bc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0a0ec1203a921be430c31150aeb1343728285f37")]
 [assembly: System.Reflection.AssemblyProductAttribute("EventBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EventBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
